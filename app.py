2b202068c73821f7b53e8d85470bf5d963a53288
Welesson Silva <welessonsilv@gmail.com>
2025-05-21 12:21:56 -0300

    corrigindo código

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ import plotly_express as px
 import streamlit as st 
 
 car_data = pd.read_csv("vehicles.csv")
-fig = px.histogram(car_data, x="year", y="price", color="make", tittle="Preço dos carros por ano")
-st.tittle("histograma de preços dos carros")
+fig = px.histogram(car_data, x="odometer")
 fig.show()
