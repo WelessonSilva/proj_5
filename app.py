ef7dc167964de9bdbe5fca5bc7cfcc615ca24c0e
Welesson Silva <welessonsilv@gmail.com>
2025-05-23 12:53:09 -0300

    App web pronto

--- app.py
+++ app.py
@@ -3,5 +3,24 @@ import plotly_express as px
 import streamlit as st 
 
 df_car = pd.read_csv('df_car.csv')
-fig = px.histogram(df_car, x="odometer", y="price", color="blue", marginal="box",)
-fig.show()
+
+st.header('Dashboard de visualização de dados')
+st.subheader('Gráfico de dispersão')
+st.write('Esse gráfico mostra a relação entre o preço e a quilometragem dos carros.')
+
+hist_button = st.button('Histograma')      
+scatter_button = st.button('Gráfico de dispersão')
+
+if hist_button:
+    st.subheader('Histograma')
+    st.write('Esse gráfico mostra a distribuição do preço dos carros.')
+    fig = px.histogram(df_car, x='price', nbins=20)
+    st.plotly_chart(fig, use_container_width=True)  
+
+    
+
+if scatter_button:
+    st.subheader('Gráfico de dispersão')
+    st.write('Esse gráfico mostra a relação entre o preço e a quilometragem dos carros.')
+    fig = px.scatter(df_car, x='price', y='model_year', color='paint_color')
+    st.plotly_chart(fig, use_container_width=True)
